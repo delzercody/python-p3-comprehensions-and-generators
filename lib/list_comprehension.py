89f214debc8098a65165f0281fa736ba28c9af7f
Cody Delzer <delzercody@gmail.com>
2023-05-27 14:00:19 -0400

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [num for num in num_list if num % 2 == 0]
+
+    return even_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence_list = [sentence + "!" for sentence in sentence_list]
+    return new_sentence_list
